ade7478ea5dd0d79775d9fd0300dccbbaa5a92fe
dhruv072 <46858749+dhruv072@users.noreply.github.com>
2020-02-22 18:32:34 +0530

    pop messsage  with js

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 
-header('location:login.html');
+
 
 session_start();
 
@@ -15,21 +15,7 @@ $connect = mysqli_connect("localhost","root","","userdata"); # connection to db
 // } # just  to check the connection between php and data  base to verify the connectionp;
 
 
-// 'if(isset($_POST['login']))
-// {
-//     $n=$_POST['nm'];# 
-//     // $i=$_POST['userid'];
-//     $p=$_POST['userpasswrord'];
-//     $a=$_POST['address'];
-      
-//     $query = "INSERT INTO `user_new`( `user_name`, `user_address`, `user_password`) VALUES ('$n','$a','$p')";
-//     $run = mysqli_query($connect,$query);
 
-//     if($run)
-//     {
-        
-//     }'
-// }
 
 
 $n=$_POST['nm'];# name of the user using for the login 
@@ -42,14 +28,32 @@ $check_rows = mysqli_num_rows($result); # check the no of rows in the database
 
 if ($check_rows == 1) #check the records in the table 
 {
-    echo " RECORD IS PRESENT  ";
+    // echo " RECORD IS PRESENT  ";
+    
+    echo "<script> 
+            alert('ALREDY REGISTERD');
+            location.href='login.html'; 
+            </script>";
+      //header('location:login.html');
+
+ 
+
+
 }
 else
 {
     $insert = " insert into user_details(user_name , user_password , user_address) values ('$n' , '$p' , '$a')";
-     mysqli_query($connect,$insert);
-     echo " new REGISTER  sucessful";
-     echo "Your ID ".$connect-> insert_id;# to show Last inserted ID
+     mysqli_query($connect,$insert);#  data inssert simple yasv
+    //  ye login pe kaam ky hua?
+     //echo "Your ID ".$connect-> insert_id;# to show Last inserted ID
+     
+
+    echo "<script> 
+            alert('REGISTERD');
+            location.href='login.html'; 
+         </script>"; #https://stackoverflow.com/questions/36967678/javascript-alert-box-not-showing-on-php-after-header-redirect/36967807
+    // header('location:login.html');
 }
+
 
 ?>
